32f19ffd917fec69cbec75ed7efa48a6b2ca0ff3
idan511 <idanalper@gmail.com>
2022-11-03 16:48:48 +0200

    done

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-contactButton = document.querySelector("#contact button");
 
-contactButton.addEventListener("click", function contactClickhandler(el) {
-  alert("Not implemented");
+dark = false
+collapse = false
+document.getElementById("dm_but").addEventListener("click", function contactClickhandler(el) {
+  if (dark) {
+    document.getElementById("body").className = ""
+  } else {
+    document.getElementById("body").className = "dark"
+  }
+  dark = !dark
+});
+
+document.getElementById("c_but").addEventListener("click", function contactClickhandler(el) {
+  cards = document.getElementsByClassName("card")
+
+  display = collapse ? "block" : "none"
+
+  for (card of cards) {
+    card.style.display = display
+  }
+
+  collapse = !collapse
 });
